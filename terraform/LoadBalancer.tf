41fa6e02950ab24c3637dd3bc5ea999a97f5fbf0
bryan aviles <easyrun32@gmail.com>
2020-12-06 18:48:51 -0500

    removing nginx code

--- terraform/LoadBalancer.tf
+++ terraform/LoadBalancer.tf
@@ -8,7 +8,10 @@ resource "aws_lb" "here-listen-alb" {
 
   enable_deletion_protection = false
 }
-//TARGET GROUP CLIENT
+
+
+
+//Target Group
 resource "aws_lb_target_group" "client" {
   name     = "here-listen-client-tg"
   port     = 80
@@ -16,7 +19,7 @@ resource "aws_lb_target_group" "client" {
   vpc_id   = aws_default_vpc.default.id
 }
 
-//TARGET GROUP USERS
+//Target Group
 resource "aws_lb_target_group" "users" {
 
   name     = "here-listen-users-tg"
@@ -28,32 +31,10 @@ resource "aws_lb_target_group" "users" {
   }
 }
 
-
-//ALL HTTP TRAFFIC WILL BE REDIRECTED TO HTTPS
 resource "aws_lb_listener" "listener_http" {
   load_balancer_arn = aws_lb.here-listen-alb.arn
   port              = "80"
   protocol          = "HTTP"
-  default_action {
-    type = "redirect"
-
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-}
-
-
-//HTTPS TRAFFIC FOWARD TO CLIENT
-resource "aws_lb_listener" "listener_https" {
-  load_balancer_arn = aws_lb.here-listen-alb.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = "arn:aws:acm:us-east-1:642815940637:certificate/406807ba-efb4-41c0-98c5-b0794a9b04e4"
-
   default_action {
     type = "forward"
 
@@ -62,9 +43,10 @@ resource "aws_lb_listener" "listener_https" {
 
 }
 
-//HTTPS TRAFFIC FOWARD TO USERS
+
 resource "aws_lb_listener_rule" "static" {
-  listener_arn = aws_lb_listener.listener_https.arn
+
+  listener_arn = aws_lb_listener.listener_http.arn
   priority     = 100
 
   action {
@@ -79,6 +61,9 @@ resource "aws_lb_listener_rule" "static" {
   }
 
 }
+
+
+
 
 
 
