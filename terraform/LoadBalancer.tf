f7e1f66279134f761649ef3d36691ef3d0ed1070
bryan aviles <easyrun32@gmail.com>
2020-12-06 16:54:26 -0500

    dns

--- terraform/LoadBalancer.tf
+++ terraform/LoadBalancer.tf
@@ -1,5 +1,4 @@
 #APPLICATION LOAD BALANCER
-
 resource "aws_lb" "here-listen-alb" {
   name               = "here-listen-alb"
   internal           = false
@@ -9,53 +8,7 @@ resource "aws_lb" "here-listen-alb" {
 
   enable_deletion_protection = false
 }
-
-
-
-
-
-
-//LISTENER for HTTPS
-# resource "aws_lb_listener" "listener_https" {
-#   load_balancer_arn = aws_lb.here-listen-alb.arn
-#   port              = "443"
-#   protocol          = "HTTPS"
-#   ssl_policy        = "ELBSecurityPolicy-2016-08"
-#   certificate_arn   = "arn:aws:acm:us-east-1:642815940637:certificate/c78aa002-1f17-45bd-8735-053bc0ac9b50"
-#   // GO TO CLIENT
-#   default_action {
-#     type = "forward"
-
-#     target_group_arn = aws_lb_target_group.client.arn
-#   }
-
-# }
-
-
-
-//LISTENER for HTTP
-//REDIRECT ALL HTTP TO HTTPS ^ UP THERE
-resource "aws_lb_listener" "listener_http" {
-  load_balancer_arn = aws_lb.here-listen-alb.arn
-  port              = "80"
-  protocol          = "HTTP"
-  default_action {
-    type = "forward"
-
-    target_group_arn = aws_lb_target_group.client.arn
-  }
-  # default_action {
-  #   type = "redirect"
-
-  #   redirect {
-  #     port        = "443"
-  #     protocol    = "HTTPS"
-  #     status_code = "HTTP_301"
-  #   }
-  # }
-}
-
-//Target Group
+//TARGET GROUP CLIENT
 resource "aws_lb_target_group" "client" {
   name     = "here-listen-client-tg"
   port     = 80
@@ -63,7 +16,7 @@ resource "aws_lb_target_group" "client" {
   vpc_id   = aws_default_vpc.default.id
 }
 
-//Target Group
+//TARGET GROUP USERS
 resource "aws_lb_target_group" "users" {
 
   name     = "here-listen-users-tg"
@@ -76,30 +29,42 @@ resource "aws_lb_target_group" "users" {
 }
 
 
-//RULES
-# resource "aws_lb_listener_rule" "static" {
-#   //was this before
-#   # listener_arn = aws_lb_listener.listener_http.arn
-#   listener_arn = aws_lb_listener.listener_https.arn
-#   priority     = 100
+//ALL HTTP TRAFFIC WILL BE REDIRECTED TO HTTPS
+resource "aws_lb_listener" "listener_http" {
+  load_balancer_arn = aws_lb.here-listen-alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type = "redirect"
 
-#   action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.users.arn
-#   }
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
 
-#   condition {
-#     path_pattern {
-#       values = ["/test", "/pingdb", "/user/login", "/user/register"]
-#     }
-#   }
 
-# }
+//HTTPS TRAFFIC FOWARD TO CLIENT
+resource "aws_lb_listener" "listener_https" {
+  load_balancer_arn = aws_lb.here-listen-alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:us-east-1:642815940637:certificate/406807ba-efb4-41c0-98c5-b0794a9b04e4"
 
+  default_action {
+    type = "forward"
+
+    target_group_arn = aws_lb_target_group.client.arn
+  }
+
+}
+
+//HTTPS TRAFFIC FOWARD TO USERS
 resource "aws_lb_listener_rule" "static" {
-  //was this before
-  # listener_arn = aws_lb_listener.listener_http.arn
-  listener_arn = aws_lb_listener.listener_http.arn
+  listener_arn = aws_lb_listener.listener_https.arn
   priority     = 100
 
   action {
